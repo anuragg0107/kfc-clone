4a8df29f83bb25b00b2ceb7fa07be62eb07039e0
SuyashaRath <104190956+SuyashaRath@users.noreply.github.com>
2022-08-27 20:54:05 +0530

    Update payment.js

--- script/payment.js
+++ script/payment.js
@@ -3,9 +3,9 @@ document.querySelector("#payed").textContent=`Payed $${totalpay}`
 
 document.querySelector("#gobackhome").addEventListener("click", homePage);
 function homePage(){
-    window.location.href="homepage.html"
+    window.location.href="index.html"
 }
 var ordervalue=JSON.parse(localStorage.getItem("order num"))
 document.querySelector("#order_id").textContent="Order-No.-#"+ordervalue;
 console.log(ordervalue)
-localStorage.removeItem("cartdata")
+localStorage.removeItem("product_data")
